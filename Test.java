80a1fe7a3127c7e87625e2a431867bef70d6cd28
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-01-19 21:20:43 +0530

    solution 999A

---
+++ Test.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Test {
+      
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Set<Integer> set=new HashSet<Integer>();
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int count=0;
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int i;
+        for(i=0;i<n;i++){
+            if(a[i]>k)break;
+            count++;
+            
+        }
+        for(int j=n-1;j>=i;j--){
+            if(a[j]>k)break;
+            count++;
+        }
+            System.out.println(count);
+        
+        
+    }
+        
+}
