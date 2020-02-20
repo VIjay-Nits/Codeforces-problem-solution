e1d99db634f218b392d4fb6c26bcb579650b8bbf
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-02-20 23:29:23 +0530

    B. Digital root Codeforces
    Educational Codeforces Round 59 (Rated for Div. 2)

---
+++ B. Digital root.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.TreeSet;
+
+public class Solution{
+       
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n=s.nextInt();
+                while(n-->0){
+                
+                    long d=s.nextLong();
+                    long gh=s.nextLong();
+                    d=(d-1)*9l+gh;
+                    System.out.println(d);
+                    
+                    
+                }
+                
+   	}     
+         
+         
+         
+}
