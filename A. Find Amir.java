b15bb852e801eb81b1bbf8d46ef531341b7dee3d
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-02-21 23:27:59 +0530

    A. Find Amir codeforces

---
+++ A. Find Amir.java
@@ -0,0 +1,28 @@
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+
+public class Solution{
+       
+	public static void main(String[] args) {
+		Scanner sdf = new Scanner(System.in);
+		
+                int t=1;
+                while(t-->0){
+                 
+                    System.out.println((sdf.nextInt()-1)/2);
+                }
+               
+        }
+   
+
+}
